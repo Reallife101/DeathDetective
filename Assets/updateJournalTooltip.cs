f49c1a61948e93b4e54ac445459efa3fd2b48ec3
Ryan Ngoon <ryan.ngoon@gmail.com>
2022-05-15 17:29:16 -0700

    Add journal update

---
+++ Assets/updateJournalTooltip.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class updateJournalTooltip : MonoBehaviour
+{
+    TMP_Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    public void playTooltip()
+    {
+        StartCoroutine(playTooltipC());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            playTooltip();
+        }
+    }
+
+    IEnumerator playTooltipC()
+    {
+        text.text = "J ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Jo ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Jou ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Jour ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journ ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journa ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal  ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal U ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal Up ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal Upd ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal Upda ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal Updat ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal Update ";
+        yield return new WaitForSeconds(0.1f);
+        text.text = "Journal Updated ";
+        yield return new WaitForSeconds(2.5f);
+        text.text = "";
+    }
+}
